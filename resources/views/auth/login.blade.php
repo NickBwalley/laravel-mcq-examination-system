07f8268883a92c36a90b0059aacefd702c5489df
NickBwalley <nickbwalley@gmail.com>
2021-12-05 22:47:31 +0300

    login and registration done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,5 +44,11 @@
                 </x-jet-button>
             </div>
         </form>
+
+        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        Register
+        </a>
+
+
     </x-jet-authentication-card>
 </x-guest-layout>
