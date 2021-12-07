6dfc5193af60f9f84398a5080896b49749a93ae2
NickBwalley <nickbwalley@gmail.com>
2021-12-07 20:03:58 +0300

    retrieve functionality completed laravel

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,42 +3,46 @@
 @section('content')
 
 <div class="container mt-3">
-
-
-    @foreach($questions as $question)
-    
-    <fieldset class="mt-3" id="question1">
-        <h4>{{$question->question}}</h4>
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="question1" id="flexRadioDefault1">
-        <label class="form-check-label" for="flexRadioDefault1">
-            {{$question->answer1}}
-        </label>
-        </div>
+    <form action="">
+        @foreach($questions as $question)
         
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="question1" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
-            {{$question->answer2}}
-        </label>
-        </div>
-        
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="question1" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
-            {{$question->answer3}}
-        </label>
-        </div>
+        <fieldset class="mt-3" id="{{$question->id}}">
+            <h4>{{$question->question}}</h4>
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="{{$question->id}}" value="1" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+                {{$question->answer1}}
+            </label>
+            </div>
+            
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="{{$question->id}}" value="2" id="flexRadioDefault2" >
+            <label class="form-check-label" for="flexRadioDefault2">
+                {{$question->answer2}}
+            </label>
+            </div>
+            
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="{{$question->id}}" value="3" id="flexRadioDefault2" >
+            <label class="form-check-label" for="flexRadioDefault2">
+                {{$question->answer3}}
+            </label>
+            </div>
 
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="question1" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
-            {{$question->answer4}}
-        </label>
-        </div>
-    </fieldset>
-    @endforeach
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="{{$question->id}}" value="4" id="flexRadioDefault2" >
+            <label class="form-check-label" for="flexRadioDefault2">
+                {{$question->answer4}}
+            </label>
+            </div>
+        </fieldset>
+        @endforeach
+
+        <button class="btn btn-primary mt-3" type="submit">finish exam</button>
+
+    </form>
 
 </div>
+    
 
 @endsection
