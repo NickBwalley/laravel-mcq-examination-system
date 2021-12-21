af684333e05710323f5b8b716b248740253d2ea5
NickBwalley <nickbwalley@gmail.com>
2021-12-21 22:51:32 +0300

    last commit. Completed MCQ Examination System

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,9 @@
     @include('addons.flash')
     <div class="alert alert-dark mt-5" role="alert">
     <h1 class="text-center">MCQ Examination System </h1> 
+    
     </div>
+    <div class="text-center alert alert-danger"> Developed by NickBwalley. All rights reserved... &copy 2021. </div>
 </div>
 
 
