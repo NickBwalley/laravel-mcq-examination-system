05a2c92a6725c1a2f957c1cbb248422c005bbb6b
NickBwalley <nickbwalley@gmail.com>
2021-12-22 16:40:21 +0300

    last commit

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -99,7 +99,7 @@ class TestsController extends Controller
 
         $points = 0;
         $percentage = 0;
-        $totalQuestions = 6;
+        $totalQuestions = 4;
 
         $subjectId = $request->input('subject_id');
 
